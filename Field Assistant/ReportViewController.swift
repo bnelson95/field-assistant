e3f8b3308a32cfa83a256eacd5d47419fe6ea28f
Blake Nelson <blake@marvin-senior.local>
2018-02-14 14:50:11 -0500

    Created and populated most view controllers on the storyboard. Have not created all the classes yet

--- Field Assistant/ReportViewController.swift
+++ Field Assistant/ReportViewController.swift
@@ -1,25 +1,24 @@
 //
-//  ViewController.swift
+//  ReportViewController.swift
 //  Field Assistant
 //
 //  Created by Blake Nelson on 2/14/18.
 //  Copyright © 2018 Blake Nelson. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ReportViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
-
