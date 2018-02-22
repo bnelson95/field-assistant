86c6c10221ba2efa3c21900f983614c11fbcd431
Blake Nelson <blake@marvin-senior.local>
2018-02-21 22:11:32 -0500

    Worked on Report views. Added camera and photo library import to ReportA. Now need to pass the image to ReportB.

--- Field Assistant/ReportBViewController.swift
+++ Field Assistant/ReportBViewController.swift
@@ -1,26 +1,29 @@
 //
-//  ViewController.swift
+//  ReportBViewController.swift
 //  Field Assistant
 //
-//  Created by Blake Nelson on 2/14/18.
+//  Created by Blake Nelson on 2/21/18.
 //  Copyright © 2018 Blake Nelson. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ReportBViewController: UIViewController {
+    
+    var myImg: UIImageView?
+    
+    @IBOutlet weak var takenImage: UIImageView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        takenImage = myImg
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
-
--- Field Assistant/ReportViewController.swift
+++ Field Assistant/ReportViewController.swift
@@ -9,11 +9,40 @@
 import Foundation
 import UIKit
 
-class ReportViewController: UIViewController {
+class ReportViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBAction func openCameraButton(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func photoFromLibrary(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            myImageView.contentMode = .scaleToFill
+            myImageView.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
